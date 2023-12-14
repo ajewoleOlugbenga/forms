ae2ca5d3a2f35af5332161ae96f70ce1410db617
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2023-12-14 16:13:59 +0100

    managing all state and in built browser components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header.jsx';
+import Login from './components/StateLogin.jsx';
+// import Signup from './components/SignUp.jsx';
+// import Login from './components/RefLogin.jsx';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Login />
+      </main>
+    </>
+  );
+}
+
+export default App;
